22ee38723012f729574b3bcb1f1a5260028de64e
SimFN <166922893+SimFN@users.noreply.github.com>
2026-01-15 10:12:11 +0100

    Update game.py

--- Project Snake/game.py
+++ Project Snake/game.py
@@ -1,1 +1,32 @@
+from classes import Snake, Fruits, GameStats
+from konstanter import WIDTH, HEIGHT, TITLE_SIZE, FPS
 
+class Game:
+    def __init__(self):
+        # Opret stats
+        self.stats = GameStats
+
+        # Opret slangen midt på banen
+        start_x = (WIDTH // TITLE_SIZE) // 2
+        start_y = (HEIGHT // TITLE_SIZE) // 2
+        self.snake = Snake((start_x, start_y))
+        
+        # Opret første frugt
+        self.fruit = Fruits(self.snake.body)
+
+        # Spillet skal køre
+        self.running = True
+
+    def reset(self):
+        # For at genstarte spillet
+        """Genstart Spillet"""
+        self.stats.reset()
+
+        start_x = (WIDTH // TITLE_SIZE) // 2
+        start_y = (HEIGHT // TITLE_SIZE) // 2
+        self.snake = Snake((start_x, start_y))
+
+        self.fruit = Fruits(self.snake.body)
+        self.running = True
+
+    def handle_input(self):
