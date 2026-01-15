f385ec87cae1b98b5755c7b308d5328bbe102734
SimFN <166922893+SimFN@users.noreply.github.com>
2026-01-15 10:03:44 +0100

    Update konstanter.py

--- Project Snake/konstanter.py
+++ Project Snake/konstanter.py
@@ -5,6 +5,7 @@ WIDTH = 800
 HEIGHT = 600
 TITLE_SIZE = 20
 FPS = 30
+SCORE = 0
 
 # Farver
 SNAKE_COLOR = (0, 255, 0)
@@ -13,17 +14,3 @@ BACKGROUND_COLOR = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
-
-score = 0
-game_window = pygame.display.set_mode((WIDTH,HEIGHT))
-
-fps = pygame.time.Clock()
-
-def show_score(choice, color, font, size):
-    
-    score_font = pygame.font.SysFont(font, size)
-
-    score_surface = score_font.render('Score : ' + str(score), True, GREEN)
-
-    
