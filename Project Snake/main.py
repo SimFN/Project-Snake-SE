fbf921da7e723ac0575bd3734daaa39dd47834a9
ERNESTO1ER1SEJ <ernestofmadsen@gmail.com>
2026-01-23 13:22:34 +0100

    Add main function to initialize game loop

--- Project Snake/main.py
+++ Project Snake/main.py
@@ -13,3 +13,27 @@ def draw_fruit(screen, fruit):
     rect = pygame.Rect(x * TITLE_SIZE, y * TITLE_SIZE, TITLE_SIZE, TITLE_SIZE)
     pygame.draw.rect(screen, FOOD_COLOR, rect)
 
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(WIDTH,HEIGHT)
+
+    clock = pygame.time.Clock()
+    game = Game()
+
+    while game.running:
+        clock.tick(FPS)
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game.running = False
+        
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    game.snake.change_direction((0,-1))
+                elif event.key == pygame.K_DOWN:
+                    game.snake.change_direction((0,1))
+                elif event.key == pygame.K_LEFT:
+                    game.snake.change_direction((-1,0))
+                elif event.key == pygame.K_RIGHT:
+                    game.snake.change_direction((1,0))
