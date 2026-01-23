ac3afcf7362ace3c2a0c9d3f20b8fe57572a7f88
SimFN <166922893+SimFN@users.noreply.github.com>
2026-01-23 13:48:57 +0100

    Update game.py

--- Project Snake/game.py
+++ Project Snake/game.py
@@ -1,10 +1,11 @@
+import pygame
 from classes import Snake, Fruits, GameStats
 from konstanter import WIDTH, HEIGHT, TITLE_SIZE, FPS
 
 class Game:
     def __init__(self):
         # Opret stats
-        self.stats = GameStats
+        self.stats = GameStats()
 
         # Opret slangen midt på banen
         start_x = (WIDTH // TITLE_SIZE) // 2
@@ -19,7 +20,6 @@ class Game:
 
     def reset(self):
         # For at genstarte spillet
-        """Genstart Spillet"""
         self.stats.reset()
 
         start_x = (WIDTH // TITLE_SIZE) // 2
@@ -30,3 +30,22 @@ class Game:
         self.running = True
 
     def handle_input(self):
+        # Direction kommer til at stamme fra main.py filen ved brug af pygame
+        # og ser ud som fx (-1, 0), (0, 1) osv.
+        self.snake.change_direction(direction)
+    
+    def update(self):
+        # Opdaterer logik pr. frame
+        
+        # Tjek om slangen sluger frugt
+        if self.snake.body[0] == self.fruit.position:
+            self.stats.increase_score()
+            self.snake.move(Grow=True)
+            self.fruit = Fruits(self.snake.body)
+        else:
+            self.snake.move(Grow=False)
+
+        # Tjek for kollisioner
+        if self.snake.collides_with_self() or self.snake.collides_with_wall():
+            self.stats.game_over = True
+            self.running = False
