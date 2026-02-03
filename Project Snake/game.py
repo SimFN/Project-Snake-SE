b9e7c7cca8a48a98a22f49731f4de909fd18285c
SimFN <166922893+SimFN@users.noreply.github.com>
2026-02-03 13:59:50 +0100

    Update game.py

--- Project Snake/game.py
+++ Project Snake/game.py
@@ -1,35 +1,35 @@
 from classes import Snake, Fruits, GameStats
-from konstanter import WIDTH, HEIGHT, TITLE_SIZE, SPEED
+from konstanter import WIDTH, HEIGHT, TITLE_SIZE
 
 class Game:
     def __init__(self):
-        # Opret stats
         self.stats = GameStats()
-
-        # Opret slangen midt på banen
-        start_x = (WIDTH // TITLE_SIZE) // 2
-        start_y = (HEIGHT // TITLE_SIZE) // 2
-        self.snake = Snake((start_x, start_y))
-        
-        # Opret første frugt
-        self.fruit = Fruits(self.snake.body)
-
-        # Spillet skal køre
+        self.state = "START"   # START, PLAYING
         self.running = True
 
-    def reset(self):
-        # For at genstarte spillet
+        self.snake = None
+        self.fruit = None
+
+    def start_game(self):
+        # Starter (eller genstarter) spillet
         self.stats.reset()
 
         start_x = (WIDTH // TITLE_SIZE) // 2
         start_y = (HEIGHT // TITLE_SIZE) // 2
         self.snake = Snake((start_x, start_y))
-
         self.fruit = Fruits(self.snake.body)
-        self.running = True
-    
+
+        self.state = "PLAYING"
+
+    def go_to_start(self):
+        # Sender spilleren tilbage til startskærmen
+        self.state = "START"
+
     def update(self):
-        # Tjek om slangen sluger frugt
+        if self.state != "PLAYING":
+            return
+
+        # Slangen spiser frugt
         if self.snake.body[0] == self.fruit.position:
             self.stats.increase_score()
             self.snake.move(Grow=True)
@@ -37,7 +37,6 @@ class Game:
         else:
             self.snake.move(Grow=False)
 
-        # Tjek for kollisioner
+        # Kollision
         if self.snake.collides_with_self() or self.snake.collides_with_wall():
-            self.stats.game_over = True
-            self.running = False
+            self.go_to_start()
